8a333e66552985b4b4940d8a4e5d350ed6fb0156
SilentChaos512 <sworkman256@gmail.com>
2019-07-04 21:25:30 -0500

    Add circuit boards, update mods.toml

--- src/main/java/net/silentchaos512/mechanisms/item/CraftingItems.java
+++ src/main/java/net/silentchaos512/mechanisms/item/CraftingItems.java
@@ -14,7 +14,8 @@ public enum CraftingItems implements IItemProvider {
     REDSTONE_ALLOY_INGOT,
     REDSTONE_ALLOY_NUGGET,
     REDSTONE_ALLOY_DUST,
-    COMPRESSED_IRON_INGOT;
+    COMPRESSED_IRON_INGOT,
+    CIRCUIT_BOARD;
 
     private final Lazy<Item> item;
 
