0c92018dff85b173cdf4f69201384040051c2c89
SilentChaos512 <sworkman256@gmail.com>
2019-09-08 21:02:42 -0500

    Start on advancements, add missing loot table

---
+++ src/main/java/net/silentchaos512/mechanisms/client/renderer/tileentity/LavaGeneratorTileRenderer.java
@@ -0,0 +1,15 @@
+package net.silentchaos512.mechanisms.client.renderer.tileentity;
+
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.silentchaos512.mechanisms.block.generator.lava.LavaGeneratorTileEntity;
+
+public class LavaGeneratorTileRenderer extends TileEntityRenderer<LavaGeneratorTileEntity> {
+    @Override
+    public void render(LavaGeneratorTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage) {
+//        FluidStack fluid = tileEntityIn.getTank().getFluidInTank(0);
+//        BlockState state = fluid.getFluid().getDefaultState().getBlockState();
+//
+//        BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+//        buffer.pos()
+    }
+}
