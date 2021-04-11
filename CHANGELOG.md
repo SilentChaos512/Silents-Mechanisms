dafeb9f3c3cb355bbf00cee5056eb286923aad4a
SilentChaos512 <sworkman256@gmail.com>
2021-04-11 15:37:36 -0500

    Fixes #139, crash with JEI. Fixes #105, canister rendering performance

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 2021-04-11
+### Fixed
+- Crash with latest JEI (7.6.3) [#139]
+- Improved rendering performance of canisters [#105]
+
 ## [1.0.0] - 2021-04-09
 ### Added
 - Config to blacklist oil lakes by dimension [#120]
--- src/main/java/net/silentchaos512/mechanisms/util/color/ColorGetter.java
+++ src/main/java/net/silentchaos512/mechanisms/util/color/ColorGetter.java
@@ -1,22 +1,43 @@
 package net.silentchaos512.mechanisms.util.color;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.fml.ModList;
+import net.silentchaos512.lib.util.NameUtils;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * TODO: Currently just leaning on JEI for this...
  */
 public final class ColorGetter {
+    private static final Cache<ResourceLocation, Integer> FLUID_COLORS = CacheBuilder.newBuilder()
+            .maximumSize(1000)
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .build();
+
     private ColorGetter() {}
 
     public static int getColor(Fluid fluid) {
+        try {
+            return FLUID_COLORS.get(NameUtils.from(fluid), () -> getFluidColor(fluid));
+        } catch (ExecutionException e) {
+            // Do nothing
+        }
+
+        return getFluidColor(fluid);
+    }
+
+    private static int getFluidColor(Fluid fluid) {
         if (fluid == Fluids.WATER) {
             return 0x0094FF;
         }
@@ -31,7 +52,7 @@ public final class ColorGetter {
 
     public static int getColor(TextureAtlasSprite sprite) {
         if (ModList.get().isLoaded("jei")) {
-            List<Integer> colors = mezz.jei.color.ColorGetter.getColors(sprite, 0xFFFFFF, 1);
+            List<Integer> colors = mezz.jei.color.ColorGetter.INSTANCE.getColors(sprite, 0xFFFFFF, 1);
             return colors.isEmpty() ? 0xFFFFFF : colors.get(0);
         }
         return 0xFFFFFF;
